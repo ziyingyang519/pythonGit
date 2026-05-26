d0c3f0785a03b547e4ac088f0c9cdbb2b839aa38
ziying Yang <abcdorange56@gmail.com>
2026-05-26 23:53:15 +0800

    last commit

--- new.py
+++ new.py
@@ -1,4 +1,2 @@
 print("Hello GitHub")
 print("This is a new file created in GitHub")
-print("This file is created to test the new branch creation and merging in GitHub")
-print("This is the end of the new file")
